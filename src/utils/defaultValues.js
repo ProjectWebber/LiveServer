1e005ae0ef829a539642e623c8039b786950020a
Juan-GB <juangabrf@gmail.com>
2025-03-03 20:00:12 -0300

    Atualizando default values.

--- src/utils/defaultValues.js
+++ src/utils/defaultValues.js
@@ -1,31 +1,31 @@
 const defaultSelectedProps = {
 	corpos: {
 		id: "corpos-m-1",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/corpos-m-1/view?project=67b909de0016b14256d8&mode=admin",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/corpos-m-1/view?project=67bf20010007bf587a10",
 	},
 	cabelos: {
 		id: "prop-vazio",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/prop-vazio/view?project=67b909de0016b14256d8",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/prop-vazio/view?project=67bf20010007bf587a10",
 	},
 	sobrancelhas: {
 		id: "prop-vazio",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/prop-vazio/view?project=67b909de0016b14256d8",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/prop-vazio/view?project=67bf20010007bf587a10",
 	},
 	olhos: {
 		id: "prop-vazio",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/prop-vazio/view?project=67b909de0016b14256d8",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/prop-vazio/view?project=67bf20010007bf587a10",
 	},
 	narizes: {
 		id: "prop-vazio",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/prop-vazio/view?project=67b909de0016b14256d8",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/prop-vazio/view?project=67bf20010007bf587a10",
 	},
 	bocas: {
 		id: "prop-vazio",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/prop-vazio/view?project=67b909de0016b14256d8",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/prop-vazio/view?project=67bf20010007bf587a10",
 	},
 	camisas: {
 		id: "prop-vazio",
-		src: "https://cloud.appwrite.io/v1/storage/buckets/67b90a58001408625cc1/files/prop-vazio/view?project=67b909de0016b14256d8",
+		src: "https://cloud.appwrite.io/v1/storage/buckets/67bf2049002d7c8e5da9/files/prop-vazio/view?project=67bf20010007bf587a10",
 	},
 };
 
